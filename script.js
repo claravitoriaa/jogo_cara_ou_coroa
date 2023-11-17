990588cb214503cc10c9fce7db3428a8fea07d99
claravitoriaa <150362854+claravitoriaa@users.noreply.github.com>
2023-11-17 09:25:49 -0300

    12 arquivo

---
+++ script.js
@@ -0,0 +1,11 @@
+let heads = 0;
+let tails = 0;
+let coin  = document.querySelector(".coin");
+let flipBtn = ocument.querySelector("#flip-button");
+let resetBtn = document.querySelector("#reset-button");
+
+flipBtn.addEventListener("click", () =>{
+ let i = Math.floor(math.random () *2);
+ coin.computedStyleMAP.animation ='none'
+ 
+})
