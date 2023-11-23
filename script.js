3b01bf8a75b0d1413f611f2daaf805f98e806bf6
claravitoriaa <150362854+claravitoriaa@users.noreply.github.com>
2023-11-23 12:17:33 -0300

    14 arquivo

--- script.js
+++ script.js
@@ -4,13 +4,34 @@ let coin  = document.querySelector(".coin");
 let flipBtn = ocument.querySelector("#flip-button");
 let resetBtn = document.querySelector("#reset-button");
 
-flipBtn.addEventListener("click", () =>{
+flipBtn.addEventListener("click", () => {
  let i = Math.floor(math.random () *2);
- coin.computedStyleMAP.animation ='none'
- if(i){
-    setTimeout(function{
+ coin.computedStyleMAP.animation = 'none'
+ if(i) {
+    setTimeout(function () {
         coin.computedStyleMAP.animation = 'spin-heads 3s forwars'
-    },100);
+    }, 100);
     heads++;
- } else{}
+ } else {  
+    setTimeout(function (){
+        coin.computedStyleMAP.animation = 'spin-tails 3s for 3s forwards'
+    }, 100);
+    tails++;
+
+ }
+   setTimeout(updateStats, 300);
+   diableButton();
+});
+
+function updateStats() {
+    document.querySelector('#heads-count').textContent= 
+    'cara: ${heads}';
+    document.querySelector('#tails-count').textContent=
+    'coroa: ${tails}';
+};
+function diableButton (){
+    flipBtn.disable = true;
+    setTimeout (function ()  {
+          flipBtn.disable =  false;
+    }, 300 );
 }
